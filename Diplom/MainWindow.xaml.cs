d94f6af80f07e25d911987b65676af0c2523ad17
Sloyka-dev <kuznetzovmarsel@yandex.ru>
2025-03-12 17:45:33 +0300

    Добавьте файлы проекта.

---
+++ Diplom/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Diplom.VoiceEngine;
+
+namespace Diplom
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Task.Run(async () =>
+            {
+
+                await Task.Delay(1000);
+                await VoiceManager.TextToSpeechAsync("В этом кратком руководстве для синтеза короткого блока введенного вами текста используется операция SpeakTextAsync. Вы также можете использовать длинный текст из файла и получить более точное управление стилями голоса, просодией и другими параметрами.");
+
+                await Task.Delay(3000);
+                await VoiceManager.SpeechToTextAsync();
+
+            });
+
+        }
+    }
+}
---
+++ Diplom/VoiceEngine/VoiceManager.cs
@@ -0,0 +1,100 @@
+﻿using System.Windows;
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+
+namespace Diplom.VoiceEngine;
+
+public static class VoiceManager
+{
+
+    static string speechKey = "4fLF1vtqJVOVCTLd60BdoAFVu57dbhq6iitO5HuEeJIadElMUnl3JQQJ99BCACYeBjFXJ3w3AAAYACOG1MxA";
+    static string speechRegion = "eastus";
+
+    static void OutputSpeechRecognitionResult(SpeechRecognitionResult speechRecognitionResult)
+    {
+        switch (speechRecognitionResult.Reason)
+        {
+            case ResultReason.RecognizedSpeech:
+                MessageBox.Show($"RECOGNIZED: Text={speechRecognitionResult.Text}");
+                break;
+            case ResultReason.NoMatch:
+                MessageBox.Show($"NOMATCH: Speech could not be recognized.");
+                break;
+            case ResultReason.Canceled:
+                var cancellation = CancellationDetails.FromResult(speechRecognitionResult);
+                MessageBox.Show($"CANCELED: Reason={cancellation.Reason}");
+
+                if (cancellation.Reason == CancellationReason.Error)
+                {
+                    MessageBox.Show($"CANCELED: ErrorCode={cancellation.ErrorCode}");
+                    MessageBox.Show($"CANCELED: ErrorDetails={cancellation.ErrorDetails}");
+                    MessageBox.Show($"CANCELED: Did you set the speech resource key and region values?");
+                }
+                break;
+        }
+    }
+
+    static void OutputSpeechSynthesisResult(SpeechSynthesisResult speechSynthesisResult, string text)
+    {
+        switch (speechSynthesisResult.Reason)
+        {
+            case ResultReason.SynthesizingAudioCompleted:
+                MessageBox.Show($"Speech synthesized for text: [{text}]");
+                break;
+            case ResultReason.Canceled:
+                var cancellation = SpeechSynthesisCancellationDetails.FromResult(speechSynthesisResult);
+                MessageBox.Show($"CANCELED: Reason={cancellation.Reason}");
+
+                if (cancellation.Reason == CancellationReason.Error)
+                {
+                    MessageBox.Show($"CANCELED: ErrorCode={cancellation.ErrorCode}");
+                    MessageBox.Show($"CANCELED: ErrorDetails=[{cancellation.ErrorDetails}]");
+                    MessageBox.Show($"CANCELED: Did you set the speech resource key and region values?");
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    public async static Task TextToSpeechAsync(string text)
+    {
+
+        var speechConfig = SpeechConfig.FromSubscription(speechKey, speechRegion);
+
+        // The neural multilingual voice can speak different languages based on the input text.
+        speechConfig.SpeechSynthesisVoiceName = "ru-RU-DmitryNeural";
+
+        /*
+         * голоса которые есть
+         * 
+         * ru-RU-SvetlanaNeural (женский)
+         * ru-RU-DmitryNeural (мужской) (норм)
+         * ru-RU-DariyaNeural (женский)
+         * 
+         */
+
+        using (var speechSynthesizer = new SpeechSynthesizer(speechConfig))
+        {
+            var speechSynthesisResult = await speechSynthesizer.SpeakTextAsync(text);
+            OutputSpeechSynthesisResult(speechSynthesisResult, text);
+        }
+
+    }
+
+    public async static Task SpeechToTextAsync()
+    {
+
+        var speechConfig = SpeechConfig.FromSubscription(speechKey, speechRegion);
+        speechConfig.SpeechRecognitionLanguage = "ru-RU";
+
+        using var audioConfig = AudioConfig.FromDefaultMicrophoneInput();
+        using var speechRecognizer = new SpeechRecognizer(speechConfig, audioConfig);
+
+        MessageBox.Show("Speak into your microphone.");
+        var speechRecognitionResult = await speechRecognizer.RecognizeOnceAsync();
+        OutputSpeechRecognitionResult(speechRecognitionResult);
+
+    }
+
+}
